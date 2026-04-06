9c5d6f4826ae35b3f73dcd75d87e9a42ff7c3cae
Wiwi Huang <kobeskskk@gmail.com>
2026-04-07 01:45:10 +0800

    feat: finish lane fitting and pitch estamation

--- libs/visualization/lane_visualization.py
+++ libs/visualization/lane_visualization.py
@@ -1,14 +1,17 @@
 import cv2
 import numpy as np
+from libs.inference.lane_fitting import get_x_at_y
 
 def draw_lane_lines(resized_image, left_lines, right_lines, save_path):
     image = np.array(resized_image)
     image_drawn_lane = image.copy()  # Python is call-by-reference. 
 
-    for x1, y1, x2, y2 in left_lines:
+    for line in left_lines:
+        x1, y1, x2, y2 = line
         cv2.line(image_drawn_lane, (x1, y1), (x2, y2), (255, 0, 0), 2, cv2.LINE_AA)  # cv2.LINE_AA for anti-aliased lines
 
-    for x1, y1, x2, y2 in right_lines:
+    for line in right_lines:
+        x1, y1, x2, y2 = line
         cv2.line(image_drawn_lane, (x1, y1), (x2, y2), (0, 255, 0), 2, cv2.LINE_AA)
 
     cv2.imwrite(save_path, cv2.cvtColor(image_drawn_lane, cv2.COLOR_RGB2BGR))
@@ -26,3 +29,66 @@ def create_overlay(resized_image, pred_mask, alpha, save_path):
     overlay = np.clip(overlay, 0, 255).astype(np.uint8)  # Ensure pixel values are valid after blending
     
     cv2.imwrite(save_path, cv2.cvtColor(overlay, cv2.COLOR_RGB2BGR))
+
+# """
+# Used to visualize the lane seed, which is the lane segmentation method 1
+# """
+# def draw_lane_seed(resized_image, left_lines, right_lines, save_path):
+#     image = np.array(resized_image)
+#     image_drawn_lane = image.copy() 
+
+#     x1, y1, x2, y2 = left_lines["seg"]
+#     cv2.line(image_drawn_lane, (x1, y1), (x2, y2), (255, 0, 0), 2, cv2.LINE_AA)  # cv2.LINE_AA for anti-aliased lines
+
+#     x1, y1, x2, y2 = right_lines["seg"]
+#     cv2.line(image_drawn_lane, (x1, y1), (x2, y2), (0, 255, 0), 2, cv2.LINE_AA)
+
+#     cv2.imwrite(save_path, cv2.cvtColor(image_drawn_lane, cv2.COLOR_RGB2BGR))
+
+# """
+# Used to visualize the k-means clustering result, which is the lane segmentation method 2
+# """
+# def draw_kmeans_clusters(resized_image, left_clusters, right_clusters, save_path):
+#     image = np.array(resized_image)
+#     image_drawn_lane = image.copy()
+
+#     left_colors  = [(255, 0, 0), (255, 255, 0)]  # Red and Yellow for left lane clusters
+#     right_colors = [(0, 0, 255), (0, 255, 0)]  # Blue and Green for right lane clusters    
+
+#     for i, cluster in enumerate(left_clusters):
+#         for line in cluster:
+#             x1, y1, x2, y2 = line["seg"]
+#             cv2.line(image_drawn_lane, (x1, y1), (x2, y2), left_colors[i], 2, cv2.LINE_AA)
+
+#     for i, cluster in enumerate(right_clusters):
+#         for line in cluster:
+#             x1, y1, x2, y2 = line["seg"]
+#             cv2.line(image_drawn_lane, (x1, y1), (x2, y2), right_colors[i], 2, cv2.LINE_AA)
+
+#     cv2.imwrite(save_path, cv2.cvtColor(image_drawn_lane, cv2.COLOR_RGB2BGR))
+
+def draw_piecewise_fits(resized_image, left_fits, right_fits, widths, save_path):
+    image = np.array(resized_image)
+    image_drawn_lane = image.copy()  # Python is call-by-reference. 
+
+    for f in left_fits:
+        y1 = int(f["y_start"])
+        y2 = int(f["y_end"])
+        x1 = int(f["slope"] * y1 + f["intercept"])
+        x2 = int(f["slope"] * y2 + f["intercept"])
+        cv2.line(image_drawn_lane, (x1, y1), (x2, y2), (255, 0, 0), 2, cv2.LINE_AA)
+    
+    for f in right_fits:
+        y1 = int(f["y_start"])
+        y2 = int(f["y_end"])
+        x1 = int(f["slope"] * y1 + f["intercept"])
+        x2 = int(f["slope"] * y2 + f["intercept"])
+        cv2.line(image_drawn_lane, (x1, y1), (x2, y2), (0, 255, 0), 2, cv2.LINE_AA)
+
+    for y, w in widths:
+            x_l = get_x_at_y(left_fits, y)
+            x_r = get_x_at_y(right_fits, y)
+            if x_l and x_r:
+                cv2.line(image_drawn_lane, (int(x_l), int(y)), (int(x_r), int(y)),
+                         (255, 255, 0), 1)
+    cv2.imwrite(save_path, cv2.cvtColor(image_drawn_lane, cv2.COLOR_RGB2BGR))
--- utils/inference_road_lane_segmentation.py
+++ utils/inference_road_lane_segmentation.py
@@ -1,11 +1,15 @@
-from libs.model.resnet101 import build_inference_model
-from libs.inference.road_segmentation import predict_road
-from libs.inference.road_segmentation import apply_road_mask
-from libs.inference.lane_segmentation import detect_lines_with_elsed
-from libs.inference.lane_segmentation import split_left_right_lines
-from libs.visualization.lane_visualization import draw_lane_lines
-from libs.visualization.lane_visualization import create_overlay
+import time
 import yaml
+from libs.model.resnet101 import build_inference_model
+from libs.inference.road_segmentation import predict_road, apply_road_mask
+from libs.inference.lane_segmentation import detect_lines_with_elsed, split_left_right_lines
+from libs.visualization.lane_visualization import draw_lane_lines, create_overlay
+# from libs.inference.lane_segmentation import cluster_left_right, get_best_seed
+# from libs.visualization.lane_visualization import draw_kmeans_clusters, draw_lane_seed
+from libs.inference.lane_fitting import collect_points_from_segments, piecewise_linear_fit, compute_lane_widths
+# from libs.inference.lane_fitting import get_x_at_y
+from libs.visualization.lane_visualization import draw_piecewise_fits
+from libs.inference.pitch_estimation import estimate_pitch_from_widths
 
 with open("config/inference_road_lane_segmentation.yaml", "r", encoding="utf-8") as f:
     config = yaml.safe_load(f)
@@ -15,24 +19,64 @@ model_path = config["model"]["model_path"]
 image_path = config["input"]["image_path"]
 resize_size = tuple(config["input"]["resize_size"])
 threshold = config["road_segmentation"]["threshold"]
+# mask_erosion_kernel = config["road_segmentation"]["mask_erosion_kernel"]
 min_slope = config["lane_segmentation"]["min_slope"]
+min_segment_length = config["lane_segmentation"]["min_segment_length"]
+# roi = config["lane_segmentation"]["roi"]
+lane_band_tolerance = config["lane_segmentation"]["lane_band_tolerance"]
 alpha = config["visualization"]["alpha"]
 save_path = config["visualization"]["save_path"]
+num_bands = config["lane_fitting"]["num_bands"]
+num_samples = config["lane_fitting"]["num_samples"]
+extra_points_per_segment = config["lane_fitting"]["extra_points_per_segment"]
+# ransac_residual_threshold = config["lane_fitting"]["ransac_residual_threshold"]
+f_x = config["pitch_estimation"]["f_x"]
+f_y = config["pitch_estimation"]["f_y"]
+w_real = config["pitch_estimation"]["w_real"]
 
 def main():
     model = build_inference_model(device, model_path)
-
+    t0 = time.perf_counter()
+    
     resized_image, pred_mask = predict_road(model, image_path, device, resize_size, threshold)
     masked_road = apply_road_mask(resized_image, pred_mask)
+    t1 = time.perf_counter()
 
-    segments = detect_lines_with_elsed(masked_road)
-    left_lines, right_lines = split_left_right_lines(segments, resized_image.width, min_slope)
+    segments = detect_lines_with_elsed(masked_road, min_segment_length)
+    t2 = time.perf_counter()
 
-    draw_lane_save_path = save_path.replace(".png", "_lanes.png")
-    draw_lane_lines(resized_image, left_lines, right_lines, draw_lane_save_path)
+    inner_left, inner_right = split_left_right_lines(segments, resized_image.width, min_slope, resized_image.height, lane_band_tolerance)
+    # left_seed = get_best_seed(left_lines, True, resized_image.height)
+    # right_seed = get_best_seed(right_lines, False, resized_image.height)
+    # left_clus, right_clus = cluster_left_right(left_lines, right_lines)
+    t3 = time.perf_counter()
+
+    left_points = collect_points_from_segments(inner_left, extra_points_per_segment)
+    right_points = collect_points_from_segments(inner_right, extra_points_per_segment)
+    left_fits = piecewise_linear_fit(left_points, num_bands)
+    right_fits = piecewise_linear_fit(right_points, num_bands)
+    widths = compute_lane_widths(left_fits, right_fits, num_samples)
+    t4 = time.perf_counter()
+
+    pitch = estimate_pitch_from_widths(widths, f_x, f_y, resized_image.height, w_real)
+    print(pitch)
+    t5 = time.perf_counter()
+
+    print(f"road segmentation:   {(t1-t0)*1000:.1f} ms")
+    print(f"line detection:      {(t2-t1)*1000:.1f} ms")
+    print(f"lane classification: {(t3-t2)*1000:.1f} ms")
+    print(f"lane fitting:        {(t4-t3)*1000:.1f} ms")
+    print(f"pitch estimation:    {(t5-t4)*1000:.1f} ms")
 
     overlay_save_path = save_path.replace(".png", "_overlay.png")
     create_overlay(resized_image, pred_mask, alpha, overlay_save_path)
-
+    draw_lane_save_path = save_path.replace(".png", "_lanes.png")
+    draw_lane_lines(resized_image, inner_left, inner_right, draw_lane_save_path)
+    # draw_lane_seed_save_path = save_path.replace(".png", "_lane_seeds.png")
+    # draw_lane_seed(resized_image, left_seed, right_seed, draw_lane_seed_save_path)
+    # draw_clus_save_path = save_path.replace(".png", "_lane_clusters.png")
+    # draw_kmeans_clusters(resized_image, left_clus, right_clus, draw_clus_save_path)
+    draw_piecewise_fits_save_path = save_path.replace(".png", "_lane_fits.png")
+    draw_piecewise_fits(resized_image, left_fits, right_fits, widths, draw_piecewise_fits_save_path)
 if __name__ == "__main__":
     main()
