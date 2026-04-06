87c8a2e14cceb6b0f00bc6302eb810e328a78f76
Wiwi Huang <kobeskskk@gmail.com>
2026-04-07 00:56:21 +0800

    feat: cancels doing erosion road_segmentation for more stable perform

--- libs/inference/road_segmentation.py
+++ libs/inference/road_segmentation.py
@@ -24,8 +24,15 @@ def predict_road(model, image_path, device, resize_size, threshold):
 
     return resized_image, pred_mask
 
-def apply_road_mask(resized_image, pred_mask):  
+def apply_road_mask(resized_image, pred_mask):
     image = np.array(resized_image)
     road_mask_255 = (pred_mask * 255).astype(np.uint8)
+
+    """
+    The erosion step is crucial to mitigate jagged boundary artifacts that can arise from the segmentation process.
+    Give up doing erosion: it will remove some lane pixels that will make the lane fitting later more difficult.
+    """
+    # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (erosion_kernel, erosion_kernel))
+    # road_mask_255 = cv2.erode(road_mask_255, kernel)  # Shrink mask inward to remove jagged boundary artifacts
     masked_road = cv2.bitwise_and(image, image, mask=road_mask_255)  # Image and Image when mask is true, else black. The cv2.bitwise_and accepts RGB images.
     return masked_road
