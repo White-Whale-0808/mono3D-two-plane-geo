2f14bc6ea749e4160d52548309a5522ca965af79
Wiwi Huang <kobeskskk@gmail.com>
2026-04-29 20:03:50 +0800

    feat: add save figure function

--- utils/plot_frameId_and_pitch.py
+++ utils/plot_frameId_and_pitch.py
@@ -2,15 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def plot_frameId_and_pitch():
-    file_path = 'inference_datasets/solid_line/up_hile/metadata.csv'
+    file_path = "outputs/metadata_gt.csv"
     df = pd.read_csv(file_path)
 
-    filtered_df = df[(df['frame_id'] >= 190) & (df['frame_id'] <= 765)]
+    df_filtered = df[df["gt_pitch_deg"].notna() & df["pred_deg"].notna()]
 
-    plt.plot(filtered_df['frame_id'], filtered_df['pitch'])
+    plt.plot(df_filtered['frame_id'], df_filtered["gt_pitch_deg"], color="blue")
+    plt.plot(df_filtered['frame_id'], df_filtered["pred_deg"], color= "red")
     plt.xlabel('frame_id')
     plt.ylabel('pitch')
 
+    plt.savefig("outputs/pitch_plot.png", dpi=150, bbox_inches='tight')
     plt.show()
 
 def main():
